bb7523bf2a7a2e0375540e585ce1f6f405c65877
Ataur Rahman <rahman.sharif39n@gmail.com>
2025-04-08 02:45:38 +0600

    io server update.

--- src/index.ts
+++ src/index.ts
@@ -24,7 +24,8 @@ const io = new Server(server, {
     cors: {
         origin: [
             'http://localhost:3000',
-            "battle-game-llm.vercel.app"
+            "battle-game-llm.vercel.app",
+            "https://battle-game-llm-flame.vercel.app"
         ]
     }
 });
