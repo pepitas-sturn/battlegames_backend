91048d819b00eeae1fe338ea78626ee8a496624d
Ataur Rahman <rahman.sharif39n@gmail.com>
2025-07-19 01:05:21 +0600

    socket server cors update

--- src/index.ts
+++ src/index.ts
@@ -24,7 +24,9 @@ const io = new Server(server, {
     cors: {
         origin: [
             'http://localhost:3000',
-            'https://game.battle-llm.io'
+            'https://game.battle-llm.io',
+            'https://shiftlayer.ai',
+            'https://battle-game-llm-c7a9.vercel.app'
         ]
     }
 });
